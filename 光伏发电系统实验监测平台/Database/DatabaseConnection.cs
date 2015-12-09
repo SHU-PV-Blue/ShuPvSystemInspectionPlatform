5b64feeaf84f72555f83974ab40d2b7d61d869bf
wolfogre <wolfogre@qq.com>
2015-12-09 22:44:58 +0800

    重用数据库相关的类

--- 光伏发电系统实验监测平台/Database/DatabaseConnection.cs
+++ 光伏发电系统实验监测平台/Database/DatabaseConnection.cs
@@ -1,12 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
 
 namespace 光伏发电系统实验监测平台.Database
 {
-	class DatabaseConnection
+	/// <summary>
+	/// 数据库连接管理
+	/// </summary>
+	static class DatabaseConnection
 	{
+		/// <summary>
+		/// 返回数据库连接
+		/// </summary>
+		/// <returns>Access数据类连接</returns>
+		static public OleDbConnection GetConnection()
+		{
+			try
+			{
+				String strConn = @"Provider = Microsoft.Jet.OLEDB.4.0;
+							Data Source=.\Database.mdb";
+				//这里采用了低版本的access数据库格式以避免不兼容的坑
+				return new OleDbConnection(strConn);
+			}
+			catch (Exception ex)
+			{
+				throw ex;
+			}
+		}
 	}
 }
