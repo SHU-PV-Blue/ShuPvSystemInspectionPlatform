d93bad69f7a130b2b06a7c6e246e0c7eb49e8119
dayang <1306693844@qq.com>
2015-12-12 23:19:32 +0800

    增加Component类和Status类

--- 光伏发电系统实验监测平台/Analyzer/ComponentAnalyzer.cs
+++ 光伏发电系统实验监测平台/Analyzer/ComponentAnalyzer.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using 光伏发电系统实验监测平台.Components;
 
 namespace 光伏发电系统实验监测平台.Analyzer
 {
-	class ComponentAnalyzer
+	class ComponentAnalyzer : Component
 	{
-#warning 需要重新写
-	}
+        public override void Analyze(Status status)
+        {
+            
+        }
+
+        public override void GetCommand()
+        {
+            
+        }
+    }
 }
