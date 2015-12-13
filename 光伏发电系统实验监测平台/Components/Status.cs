79b3f3f08e61782fe8fad28c19fd234b512f54bf
dayang <1306693844@qq.com>
2015-12-13 17:03:29 +0800

    添加status注释

--- 光伏发电系统实验监测平台/Components/Status.cs
+++ 光伏发电系统实验监测平台/Components/Status.cs
@@ -9,11 +9,35 @@ namespace 光伏发电系统实验监测平台.Components
 {
     class Status
     {
+
+        /// <summary>
+        /// 时间
+        /// </summary>
         public DateTime Time { get; set; }
+
+        /// <summary>
+        /// 消息队列
+        /// </summary>
         public List<KeyValuePair<byte, bool>> MessageQueue { get; set; }
+
+        /// <summary>
+        /// 数据库连接
+        /// </summary>
         public OleDbConnection OleDbCon { get;set; }
+
+        /// <summary>
+        /// 组件号
+        /// </summary>
         public int ComponentId { get; set; }
+
+        /// <summary>
+        /// 方位角
+        /// </summary>
         public double Azimuth { get; set; }
+
+        /// <summary>
+        /// 倾角
+        /// </summary>
         public double Obliquity { get; set; }
     }
 }
