281bf0234b1a7c0d8b55052efdaf432f16d1958c
Jason Song <wolfogre@outlook.com>
2015-12-13 16:19:18 +0800

    添加串口监听

--- 光伏发电系统实验监测平台/Manager/Transceiver.cs
+++ 光伏发电系统实验监测平台/Manager/Transceiver.cs
@@ -32,6 +32,7 @@ namespace 光伏发电系统实验监测平台.Manager
 		public Transceiver(SerialPort serialPort)
 		{
 			_serialPort = serialPort;
+			_serialPort.DataReceived += DataReceivedHandler;
 		}
 
 		public void Start(Command[] commands, int cycle)
@@ -68,7 +69,17 @@ namespace 光伏发电系统实验监测平台.Manager
 				_sendTread.Abort();
 			if (_serialPort != null && _serialPort.IsOpen)
 				_serialPort.Close();
-			//TODO:设置串口与电机通信，查询电机停转指令，停转电机
+			int sendCount = 3;
+			while(sendCount-- > 0)
+			{
+				_serialPort.BaudRate = 9600;
+				_serialPort.Open();
+				byte[] bytes = (new Relay32()).GetCommand("停转");
+				_serialPort.Write(bytes, 0, bytes.Length);
+				_serialPort.Close();
+			}
+
+
 		}
 
 		public void Reset()
