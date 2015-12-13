9dcac7d72e19001368becf7d54fa3c53470ee9c2
zhaokuo <1014336691@qq.com>
2015-12-13 14:18:13 +0800

    加特效

--- 光伏发电系统实验监测平台/Properties/Resources.Designer.cs
+++ 光伏发电系统实验监测平台/Properties/Resources.Designer.cs
@@ -199,5 +199,15 @@ namespace 光伏发电系统实验监测平台.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
+        /// </summary>
+        internal static System.Drawing.Bitmap 实验平台图 {
+            get {
+                object obj = ResourceManager.GetObject("实验平台图", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
